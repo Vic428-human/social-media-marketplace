5a02501816261cbb0047adb25c7f4ad6bcf61355
Vic428-human <z0983195379a@gmail.com>
2026-02-04 19:36:18 +0800

    feat: 熱門商品切版

---
+++ client/src/components/LatestListing.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Title from "./Title";
+
+const LatestListing = () => {
+  return (
+    <div className="mt-20 mb-8">
+      <Title
+        title="熱門商品"
+        desciption="最炙手可熱的商品上架囉，快來看看吧~"
+      />
+    </div>
+  );
+};
+
+export default LatestListing;
