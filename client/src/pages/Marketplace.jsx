97e666a4b83c480ad72ab6e893479415ac45a830
Vic428-human <z0983195379a@gmail.com>
2026-02-06 18:29:45 +0800

    feat: 把高價單品旋轉木馬放在交易市集最上層

--- client/src/pages/Marketplace.jsx
+++ client/src/pages/Marketplace.jsx
@@ -8,12 +8,90 @@ const Marketplace = () => {
   const navigator = useNavigate();
   const { listings } = useSelector((state) => state.listing);
   const [showFilter, setShowFilter] = React.useState(false);
+
+  const quantity = 10;
+
+  const items = [
+    {
+      position: 2,
+      src: "https://rd.fharr.com/images/tw/card/4121/",
+      alt: "皮里恩",
+    },
+    {
+      position: 1,
+      src: "https://rd.fharr.com/images/tw/card/4168/",
+      alt: "黑暗之王",
+    },
+    {
+      position: 3,
+      src: "https://rd.fharr.com/images/tw/card/4302/",
+      alt: "塔奧群卡",
+    },
+    {
+      position: 4,
+      src: "https://rd.fharr.com/images/tw/card/4241/",
+      alt: "聖天使波利卡片",
+    },
+    {
+      position: 5,
+      src: "https://rd.fharr.com/images/tw/card/4131/",
+      alt: "月夜貓卡片",
+    },
+    {
+      position: 6,
+      src: "https://rd.fharr.com/images/tw/card/4306/",
+      alt: "蛙王",
+    },
+    {
+      position: 7,
+      src: "https://rz.fharr.com/images/tw/card/4123/",
+      alt: "虎王卡片",
+    },
+    {
+      position: 8,
+      src: "https://rd.fharr.com/images/tw/card/4144/",
+      alt: "俄塞里斯卡片",
+    },
+    {
+      position: 9,
+      src: "https://rd.fharr.com/images/tw/card/4143/",
+      alt: "獸人英雄卡片",
+    },
+    {
+      position: 10,
+      src: "https://rd.fharr.com/images/tw/card/4236/",
+      alt: "古埃及王卡片",
+    },
+  ];
   // 認證過的優先顯示
   const sortedListings = [...listings].sort((a, b) => b.verified - a.verified);
   return (
-    <div className="px-6 md:px-16 lg:px-24 xl:px-32">
+    <div className="flex flex-col px-6 md:px-16 lg:px-24 xl:px-32">
+      {/* 超稀有卡片區 */}
+      <div className="w-full h-[300px] text-center overflow-hidden relative">
+        <div
+          className="banner-slider absolute w-[75px] h-[100px] top-[15%] left-[calc(50%-100px)]
+                   [transform-style:preserve-3d] [transform:perspective(1000px)]"
+          style={{ ["--quantity"]: quantity }}
+        >
+          {items.map((it) => (
+            <div
+              key={it.position}
+              className="absolute inset-0
+                       [transform:rotateY(calc((var(--position)-1)*(360deg/var(--quantity))))_translateZ(550px)]"
+              style={{ ["--position"]: it.position }}
+            >
+              <img
+                className="w-full h-full object-cover"
+                src={it.src}
+                alt={it.alt}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
       {/* 上半段 */}
-      <div className="flex items-center justify-between text-slate-500">
+      <div className="-mt-24 flex items-center justify-between text-slate-500">
         <button
           onClick={() => {
             navigator("/");
